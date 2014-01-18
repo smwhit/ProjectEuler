02c1b09acbd72346c4c1bfc7101868a692613fec
Simon Whittemore <simon.whittemore@gmail.com>
2014-01-18 18:24:10 +0000

    problem 1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace project_euler
     {
         static void Main(string[] args)
         {
+            problem1_multiples.SumOfMultiplesOf3And5Below1000();
+
             Console.WriteLine("Prime factors of {1} are {0}", string.Join(" ", problem_3_prime_factors.PrimeFactors(13195)), 13195);
             Console.WriteLine("Prime factors of {1} are {0}", string.Join(" ", problem_3_prime_factors.PrimeFactors(600851475143)), 600851475143);
 
---
+++ problem1_multiples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace project_euler
+{
+    public class problem1_multiples
+    {
+        public static void SumOfMultiplesOf3And5Below1000()
+        {
+            var multiples = new List<int>();
+
+            for (var i = 0; i < 1000; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                {
+                    multiples.Add(i);
+                }
+            }
+
+            Console.WriteLine(multiples.Sum());
+        }
+        
+    }
+}
